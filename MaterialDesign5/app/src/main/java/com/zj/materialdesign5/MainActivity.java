b89ec6588094e24f3fe6b832fa68edb882cab31e
whuhan2013 <whuhan2013@163.com>
2016-04-08 17:56:09 +0800

    material

---
+++ MaterialDesign5/app/src/main/java/com/zj/materialdesign5/MainActivity.java
@@ -0,0 +1,116 @@
+package com.zj.materialdesign5;
+
+import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
+        CollapsingToolbarLayout mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        mCollapsingToolbarLayout.setTitle("MyCollapsingToolbar");
+        //通过CollapsingToolbarLayout修改字体颜色
+        mCollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
+        mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.GREEN);//设置收缩后Toolbar上字体的颜色
+
+
+        //设置ViewPager
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(mViewPager);
+
+//给TabLayout增加Tab, 并关联ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("内容简介"));
+        tabLayout.addTab(tabLayout.newTab().setText("作者简介"));
+        tabLayout.addTab(tabLayout.newTab().setText("目录"));
+        tabLayout.setupWithViewPager(mViewPager);
+
+
+    }
+
+    private void setupViewPager(ViewPager mViewPager) {
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(DetailFragment.newInstance(getAsset("book_content.txt")), "内容简介");
+        adapter.addFragment(DetailFragment.newInstance(getAsset("book_author.txt")), "作者简介");
+        adapter.addFragment(DetailFragment.newInstance(getAsset("book_menu.txt")), "目录");
+        mViewPager.setAdapter(adapter);
+    }
+
+    private String getAsset(String fileName) {
+        AssetManager am = getResources().getAssets();
+        InputStream is = null;
+        try {
+            is = am.open(fileName, AssetManager.ACCESS_BUFFER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Scanner(is).useDelimiter("\\Z").next();
+    }
+
+
+    static class MyPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
+
+
+
+
+
+}
